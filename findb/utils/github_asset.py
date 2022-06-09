c8347856e1c82cc5ca534ef0e617dd8abffef5fe
adagrad <adagrad+github@protonmail.com>
2022-06-09 18:41:06 +0000

    add task dependency

--- findb/utils/github_asset.py
+++ findb/utils/github_asset.py
@@ -1,7 +1,10 @@
 import os.path
+import re
 import subprocess
 import sys
 import shutil
+from datetime import datetime
+
 import requests
 
 
@@ -64,6 +67,7 @@ def download_file(repo, tag, token, asset_name, path='.'):
 
     return path
 
+
 def upload_file(repo, tag, token, asset_file):
     asset_name = os.path.basename(asset_file)
     release, assets = list_release_files(repo, tag, token)
@@ -93,18 +97,64 @@ def upload_file(repo, tag, token, asset_file):
     return resp.json()
 
 
+def copy_release_files(repo, tag_src, tag_dest, token, replace_existing=False):
+    src_release, src_assets = list_release_files(repo, tag_src, token)
+    tgt_release, tgt_assets = list_release_files(repo, tag_dest, token)
+
+    if not replace_existing:
+        existing_files = [a['name'] for a in tgt_assets]
+        src_assets = [a for a in src_assets if a['name'] not in existing_files]
+
+    for src in src_assets:
+        file = download_file(repo, tag_src, token, src['name'])
+        if os.path.exists(file):
+            upload_file(repo, tag_dest, token, src['name'])
+            os.remove(file)
+        else:
+            print("error file not found", file)
+
+
+def bump_year(repo, tag, token, file):
+    year_ex = re.compile("\d{4}")
+    release, assets = list_release_files(repo, tag, token)
+    file_extensions = [".db", ".tgz", ".csv"]
+    for asset in assets:
+        for ext in file_extensions:
+            file_no_ext = file.replace(ext, '')
+
+        if asset['name'].startswith(file_no_ext):
+            download_file(repo, tag, token, asset['name'])
+            # separate asset name and year.
+            years = [int(y) for y in year_ex.findall(asset['name'])]
+            # find max year or empty
+            year = max(years) if len(years) > 0 else None
+            # rename file and upload with new year
+            new_name = asset['name'].replace(str(year), str(year + 1)) if year is not None else asset['name'].replace(file_no_ext, f"{file_no_ext}{datetime.now().year}")
+
+            print("rename", asset['name'], new_name)
+            os.rename(asset['name'], new_name)
+            upload_file(repo, tag, token, new_name)
+
+
 if __name__ == '__main__':
     action = sys.argv[1]
     token = sys.argv[2]
     repo = sys.argv[3]
     tag = sys.argv[4]
-    assets = sys.argv[5:]
+    files = sys.argv[5:]
 
-    for asset in assets:
+    for file in files:
         if action == 'GET':
-            download_file(repo, tag, token, asset)
+            download_file(repo, tag, token, file)
         elif action == 'PUT':
-            upload_file(repo, tag, token, asset)
+            upload_file(repo, tag, token, file)
+        elif action == 'BUMP_YEAR':
+            bump_year(repo, tag, token, file)
+        elif action == 'COPY':
+            stag, ttag = tag.split("|")
+            copy_release_files(repo, stag, ttag, False)
+        else:
+            print("unknown action", action, file)
 
 
 def test_list_files():
@@ -121,3 +171,11 @@ def test_download():
 
     download_file('adagrad/findb', '2021', open("../../.token").read(), "test.csv", "/tmp/")
     assert os.path.exists('/tmp/test.csv')
+
+
+def test_bump_year():
+    bump_year('adagrad/findb', '2021', open("../../.token").read(), "test.csv")
+
+
+def test_copy():
+    copy_release_files('adagrad/findb', 'db', '2021', open("../../.token").read())
